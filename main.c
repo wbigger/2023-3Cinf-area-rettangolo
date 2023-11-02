a66cdfbafca2a253f809bcb1712f38679bbf8006
Claudio <wbigger@gmail.com>
2023-11-02 17:57:08 +0100

    Add assert

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 int calcola_area_rettangolo(int base, int altezza) {
     return base*base;
@@ -7,5 +8,11 @@ int calcola_area_rettangolo(int base, int altezza) {
 int main() {
     int area = calcola_area_rettangolo(10,20);
     printf("L'area del rettangolo e': %d",area);
+
+    // Test
+    assert(calcola_area_rettangolo(10,20)==200);
+
+    printf("Tutti i test sono andati a buon fine");
+    return 0;
 }
 
